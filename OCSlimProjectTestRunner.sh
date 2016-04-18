92efde11a4fa0b3bb56655052300ac49dc86bc55
Richard Moult <richardmoult75@gmail.com>
2016-04-18 16:48:35 +0100

    Adding Fitnesse Acceptance Testing Framework To Project.  To Run, in terminal go to the project root folder and enter ./LaunchFitnesse

---
+++ OCSlimProjectTestRunner.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/Users/richardmoult/Library/Developer/Xcode/DerivedData/ListOfHotels-drlkryelzyywsofihnyoapzwlqvv/Build/Products/Debug-iphonesimulator/AcceptanceTests.app/OCSlimProject.bundle/RunTestsTargetWithSlimPort $1
