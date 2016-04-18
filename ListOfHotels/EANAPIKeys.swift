365611190b896b458e3c2f46a8109b9d4d3a1b5b
Maciej Matyjas <mmatyjas@expedia.com>
2016-04-18 16:16:52 +0100

    two passing tests, focused on hiding them EAN API Keys first, then doing the networking

---
+++ ListOfHotels/EANAPIKeys.swift
@@ -0,0 +1,20 @@
+//
+//  EANAPIKeys.swift
+//  ListOfHotels
+//
+//  Created by Maciej Matyjas on 4/18/16.
+//  Copyright © 2016 Expedia. All rights reserved.
+//
+
+import Foundation
+
+public struct EANAPIKeys {
+    
+    init(fileNamed: String) {
+        
+    }
+    
+    func getClientId() -> String {
+        return ""
+    } 
+}
---
+++ ListOfHotelsTests/EANAPIKeyTests.swift
@@ -0,0 +1,34 @@
+//
+//  EANAPIKeyTests.swift
+//  ListOfHotels
+//
+//  Created by Maciej Matyjas on 4/18/16.
+//  Copyright © 2016 Expedia. All rights reserved.
+//
+
+import XCTest
+
+class EANAPIKeyTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testEANAPIKeyExists() {
+        let keys = EANAPIKeys(fileNamed: "")
+        let clientID = keys.getClientId();
+        XCTAssertNotNil(clientID);
+    }
+
+    func testEANAPIKeysAcceptsFile() {
+        let keys = EANAPIKeys(fileNamed: "some-file.plist");
+        XCTAssertNotNil(keys.getClientId())
+    }
+
+}
