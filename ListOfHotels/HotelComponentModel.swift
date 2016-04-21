25d6de70685a690d54cf0e3cb48b41f974945f3e
Lawrence Forooghian <lforooghian@hotels.com>
2016-04-21 11:05:05 +0100

    Add Swift file that was missed because of my dodgy .gitignore

---
+++ ListOfHotels/HotelComponentModel.swift
@@ -0,0 +1,14 @@
+//
+//  HotelComponentModel.swift
+//  ListOfHotels
+//
+//  Created by Lawrence Forooghian on 20/04/2016.
+//  Copyright © 2016 Expedia. All rights reserved.
+//
+
+import Foundation
+
+class HotelComponentModel: NSObject {
+    var latitude: Double = 0
+    var longitude: Double = 0
+}
